383d845dc086c6b8ea03cd203937bef7137bcf42
MariosMarinos <38200565+MariosMarinos@users.noreply.github.com>
2019-04-22 16:41:00 +0300

    Update ServerProtocol.java

--- Pdc_LAB2Assignment/src/ServerProtocol.java
+++ Pdc_LAB2Assignment/src/ServerProtocol.java
@@ -10,18 +10,17 @@ public class ServerProtocol {
 	
 	public String processRequest(String theInput) {
 		String theOutput = null;
-		System.out.println("Received message from client: " + theInput);
 		String[] Parts = theInput.split(" "); // Splitting with spaces so it's necessary to have spaces between code,state and time in order 
 		// to make the protocol work otherwise it will crush.
-		if (Parts[0].startsWith("READ")) {
+		if (Parts[0].startsWith("READ")) { // if the request starts with read and code exists return ROK the code and the flight's infos.
 			if (DetailsTable.containsKey(Parts[1])){
 				theOutput = "ROK " + Parts[1] + " " + DetailsTable.get(Parts[1]);
 			}
 			else {
-				theOutput = "RERR";
+				theOutput = "RERR"; //otherwise it prints RERR
 			}
 		}
-		else if (Parts[0].startsWith("WRITE")){
+		else if (Parts[0].startsWith("WRITE")){//if the request starts with WRITE and the needed infos it writes it into the map or if it exists already overwrites it.
 			String State = Parts[2];
 			String Time = Parts[3];
 			ArrayList<String> Temp = new ArrayList<String>();
@@ -30,18 +29,17 @@ public class ServerProtocol {
 			DetailsTable.put(Parts[1],Temp);
 			theOutput = "WOK" ;
 		}
-		else if (Parts[0].startsWith("REMOVE")) {
+		else if (Parts[0].startsWith("REMOVE")) { //if the request starts with REMOVE and an already existed code it removes it.
 			if (DetailsTable.containsKey(Parts[1])) {
-				// 5 secs for deleting an element.
 				DetailsTable.remove(Parts[1]);
 				theOutput = "SUCCESSFULLY REMOVED";
 			}
-			else {
-				theOutput = "THERE IS NO ELEMEMNT WITH THIS KEY";
+			else { //if the request tries to remove something which is not in the HashMap print no flight with this code to remove.
+				theOutput = "NO FLIGHT WITH THIS CODE TO REMOVE";
 			}
 		}
-		else {
-			theOutput = "THERE IS NO COMMAND AS THIS";
+		else {//if the request starts with something that server doesn't know print UNKNOWN COMMAND.
+			theOutput = "UNKNOWN COMMAND";
 		}
 		return theOutput;
 	}
